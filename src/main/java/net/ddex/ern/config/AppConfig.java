19e1c57ff3f558f13a0ff9c8a0bb07bb41144d8b
elibeta22 <ebeta9222gmail.com>
2017-07-25 15:44:01 -0400

    Embedded Jetty

---
+++ src/main/java/net/ddex/ern/config/AppConfig.java
@@ -0,0 +1,14 @@
+package net.ddex.ern.config;
+
+/**
+ * Created by eli on 7/19/2017.
+ */
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "net.ddex.ern")
+public class AppConfig {
+}
+
---
+++ src/main/java/net/ddex/ern/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package net.ddex.ern.config;
+
+/**
+ * Created by eli on 7/19/2017.
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("images/**").addResourceLocations("images/");
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setDefaultEncoding("UTF-8");
+        multipartResolver.setMaxUploadSize(-1);
+        return multipartResolver;
+    }
+
+}
