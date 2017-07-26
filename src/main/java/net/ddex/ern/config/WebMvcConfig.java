d9d831a90a8b3d41672d0dffe4f923ed20702697
elibeta22 <ebeta9222gmail.com>
2017-07-26 18:13:57 -0400

    fixture

--- src/main/java/net/ddex/ern/config/WebMvcConfig.java
+++ src/main/java/net/ddex/ern/config/WebMvcConfig.java
@@ -8,17 +8,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("images/**").addResourceLocations("images/");
-    }
 
     @Bean
     public CommonsMultipartResolver multipartResolver(){
